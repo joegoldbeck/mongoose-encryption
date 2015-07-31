95fb6099836f382eb9b53474308c1b2323bd686e
Jacob Burton <burtonjc35@gmail.com>
2015-07-31 13:59:00 -0800

    Don't load encryptedChildren plugin for mongoose 4.1.1 and later

--- index.js
+++ index.js
@@ -459,6 +459,11 @@
   // which could otherwise cause data loss if validation error fixed and a resave was attempted
   // For use in conjunction with the main encryption plugin
   module.exports.encryptedChildren = function(schema, options) {
+    if (mongoose.version > '4.1.0') {
+      console.warn('encryptedChildren plugin is not needed for mongoose versions above 4.1.1, continuing without plugin.');
+      return;
+    }
+
     schema.post('validate', function(doc) {
       if (doc.errors) {
         decryptEmbeddedDocs(doc);
